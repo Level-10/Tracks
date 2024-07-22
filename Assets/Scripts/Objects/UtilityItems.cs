4e4c87846d2a0af5461ea3e4a6a34d3f72236cd5
Level-10 <153094317+Level-10@users.noreply.github.com>
2024-07-22 17:09:19 +0200

    Start UI Inventory
    J'aioublié mais la dernière fois j'ai ajouté des struct et aussi des Singletons

--- Assets/Scripts/Objects/UtilityItems.cs
+++ Assets/Scripts/Objects/UtilityItems.cs
@@ -2,34 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public enum KeyTypes
+public enum UtilityItemsTypes
 {
     // When use enum for chose th type, make a .ToString() for set litteral enum as a string
-    SmallKey,
-    MediumKey,
-    GoldKey,
-    key
+    Wrench
 }
 
-public class Keys : Pickups
+public class UtilityItems : Pickups
 {
-    [SerializeField] KeyTypes keyType = KeyTypes.key;
+    [SerializeField] UtilityItemsTypes itemType = UtilityItemsTypes.Wrench;
 
     protected override void Start()
     {
         base.Start();
     }
 
-
     protected override void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
-            //Player _player = collision.GetComponent<Player>();
             // Set name par rapport à l'enum selectionné
-            ItemsStruct _item = new ItemsStruct(keyType.ToString(), 1);
+            ItemsStruct _item = new ItemsStruct(itemType.ToString(), 1);
             Inventory.Instance.AddItem(_item);
             Destroy(gameObject);
         }
+
     }
 }
